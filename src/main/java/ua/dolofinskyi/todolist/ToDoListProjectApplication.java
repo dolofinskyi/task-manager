ff78636cc02a108191dc9a00592e47b86c2de834
dolofinskyi <denis.olofinskyi@gmail.com>
2024-07-28 10:43:22 +0300

    Initial commit

---
+++ src/main/java/ua/dolofinskyi/todolist/ToDoListProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.dolofinskyi.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListProjectApplication.class, args);
+	}
+
+}
