9959968c88332d36f7514686d0b7be17c8435062
dolofinskyi <denis.olofinskyi@gmail.com>
2024-07-29 13:38:33 +0300

    Create Task feature

---
+++ app/src/main/java/ua/dolofinskyi/features/task/Task.java
@@ -0,0 +1,8 @@
+package ua.dolofinskyi.features.task;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Task {
+
+}
---
+++ app/src/main/java/ua/dolofinskyi/features/task/TaskRepository.java
@@ -0,0 +1,9 @@
+package ua.dolofinskyi.features.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
