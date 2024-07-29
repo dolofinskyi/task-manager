8c399e8d59801948f7cc9a5178f53024649c6751
dolofinskyi <denis.olofinskyi@gmail.com>
2024-07-29 15:45:06 +0300

    Config task feature

--- app/src/main/java/ua/dolofinskyi/features/task/Task.java
+++ app/src/main/java/ua/dolofinskyi/features/task/Task.java
@@ -1,8 +1,16 @@
 package ua.dolofinskyi.features.task;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "task")
 public class Task {
-
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private String description;
 }
