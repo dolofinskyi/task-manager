1f0035390ec68dbf7fe0e5fe7fc2e4a00bf2be95
dolofinskyi <denis.olofinskyi@gmail.com>
2024-07-30 13:07:03 +0300

    Update task feature

--- app/src/main/java/ua/dolofinskyi/features/task/TaskServiceImpl.java
+++ app/src/main/java/ua/dolofinskyi/features/task/TaskServiceImpl.java
@@ -3,12 +3,23 @@ package ua.dolofinskyi.features.task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.StreamSupport;
+
 @Service
 public class TaskServiceImpl implements TaskService {
     @Autowired
     private TaskMapper taskMapper;
     @Autowired
     private TaskRepository taskRepository;
+
+    @Override
+    public TaskDto getTaskById(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found."));
+        return taskMapper.toDto(task);
+    }
 
     @Override
     public TaskDto createTask(TaskDto taskDto) {
@@ -36,5 +47,13 @@ public class TaskServiceImpl implements TaskService {
             throw new IllegalArgumentException("ID must be provided.");
         }
         taskRepository.deleteById(id);
+    }
+
+    @Override
+    public List<TaskDto> getAllTasks() {
+        Spliterator<Task> taskSpliterator = taskRepository.findAll().spliterator();
+        return StreamSupport.stream(taskSpliterator, false)
+                .map(task -> taskMapper.toDto(task))
+                .toList();
     }
 }
