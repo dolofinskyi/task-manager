8a496118c9bbb9e05b851c97f6553eee5545d3f1
dolofinskyi <denis.olofinskyi@gmail.com>
2024-07-30 12:38:01 +0300

    Create TaskServiceImpl test

---
+++ app/src/test/java/ua/dolofinskyi/features/task/TaskServiceImplTest.java
@@ -0,0 +1,46 @@
+package ua.dolofinskyi.features.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("h2")
+class TaskServiceImplTest {
+    @Autowired
+    private TaskServiceImpl taskService;
+    private TaskDto initTask;
+
+    @BeforeEach
+    void setUp() {
+        initTask = new TaskDto();
+        initTask.setTitle("Title");
+        initTask.setDescription("Description");
+    }
+
+    @Test
+    void testCreateTask() {
+        TaskDto createdTask = taskService.createTask(initTask);
+        assertEquals(initTask.getTitle(), createdTask.getTitle());
+        assertEquals(initTask.getDescription(), createdTask.getDescription());
+    }
+
+    @Test
+    void testUpdateTask() {
+        TaskDto createdTask = taskService.createTask(initTask);
+        TaskDto updatedTask = taskService.updateTask(createdTask);
+        assertEquals(createdTask.getId(), updatedTask.getId());
+        assertEquals(createdTask.getTitle(), updatedTask.getTitle());
+        assertEquals(createdTask.getDescription(), updatedTask.getDescription());
+    }
+
+    @Test
+    void testDeleteTask() {
+        TaskDto createdTask = taskService.createTask(initTask);
+        assertDoesNotThrow(() -> taskService.deleteTask(createdTask.getId()));
+    }
+}
