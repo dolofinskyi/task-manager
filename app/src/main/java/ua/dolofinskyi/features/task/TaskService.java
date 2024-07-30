cddf4006a50c1879174ac166bb0f9d0429d7868d
Denys Olofinskyi <132900577+dolofinskyi@users.noreply.github.com>
2024-07-30 13:13:08 +0300

    Update TaskService.java

--- app/src/main/java/ua/dolofinskyi/features/task/TaskService.java
+++ app/src/main/java/ua/dolofinskyi/features/task/TaskService.java
@@ -3,7 +3,6 @@ package ua.dolofinskyi.features.task;
 import java.util.List;
 
 public interface TaskService {
-
     TaskDto getTaskById(Long id);
     TaskDto createTask(TaskDto taskDto);
     TaskDto updateTask(TaskDto taskDto);
