86de1700ad8161ca412ab5def2ebafb17a88ba9e
dolofinskyi <denis.olofinskyi@gmail.com>
2024-07-31 13:56:31 +0300

    Create TaskExceptionHandler

---
+++ app/src/main/java/ua/dolofinskyi/features/task/TaskExceptionHandler.java
@@ -0,0 +1,26 @@
+package ua.dolofinskyi.features.task;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ua.dolofinskyi.features.response.Response;
+import ua.dolofinskyi.features.task.exception.TaskMissingIdException;
+import ua.dolofinskyi.features.task.exception.TaskNotFoundException;
+
+@RestControllerAdvice
+public class TaskExceptionHandler {
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<Response> handleTaskNotFoundException() {
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        Response response = new Response(httpStatus.getReasonPhrase(), httpStatus.value());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TaskMissingIdException.class)
+    public ResponseEntity<Response> handleTaskMissingIdException() {
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        Response response = new Response(httpStatus.getReasonPhrase(), httpStatus.value());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
