b2b95dab2fb58fb9ed12e6d60981e11053a73220
dolofinskyi <denis.olofinskyi@gmail.com>
2024-08-04 17:12:08 +0300

    Add user exceptions

--- app/src/main/java/ua/dolofinskyi/features/user/UserService.java
+++ app/src/main/java/ua/dolofinskyi/features/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public interface UserService {
     User getUserByOAuth2Sub(String oauth2Sub);
+    User getUserFromSecurityContextHolder();
     User createUser(OAuth2User oAuth2User);
     UserDto updateUser(UserDto userDto);
     void deleteUser();
