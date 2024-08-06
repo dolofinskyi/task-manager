70aa9387eee8baa522ec8737a3233054e621c8e6
dolofinskyi <denis.olofinskyi@gmail.com>
2024-08-06 19:29:55 +0300

    Perform app test view

--- app/src/main/java/ua/dolofinskyi/common/controller/AppController.java
+++ app/src/main/java/ua/dolofinskyi/common/controller/AppController.java
@@ -1,13 +1,34 @@
 package ua.dolofinskyi.common.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.dolofinskyi.features.task.TaskDto;
+import ua.dolofinskyi.features.user.UserDto;
+import ua.dolofinskyi.features.user.UserServiceImpl;
+
+import java.util.ArrayList;
 
 @Controller
 public class AppController {
+    @Autowired
+    private UserServiceImpl userService;
+
     @GetMapping("/app")
     public String app(Model model) {
+        UserDto userDto = userService.getUserDtoFromSecurityContextHolder();
+
+        for (int i = 0; i < 5; i++) {
+            TaskDto taskDto = new TaskDto();
+            taskDto.setId((long) i);
+            taskDto.setTitle("Title %d".formatted(i));
+            taskDto.setDescription("Description %d".formatted(i));
+            taskDto.setIsDone(i % 2 == 0);
+            userDto.getTasks().add(taskDto);
+        }
+
+        model.addAttribute("user", userDto);
         return "app";
     }
 }
